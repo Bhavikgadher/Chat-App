8f2c338f051a9280aaf4f9c521c95c7b6d02ed9d
Bhavikgadher <gadherbhavik@gmail.com>
2025-04-27 16:19:15 +0530

    update configuration file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
@@ -13,7 +17,6 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'ChatApp',
       theme: ThemeData().copyWith(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 13, 146, 244),
         ),
